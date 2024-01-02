99aef9535a4465f12da77a0b907d06427df36ee4
AlfiyaSainudeen <154522933+AlfiyaSainudeen@users.noreply.github.com>
2024-01-02 14:01:41 +0530

    Add files via upload

---
+++ AssignmentDay6/Question3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay6
+{
+    internal class Question3
+    {
+        static void Main(string[] args)
+        {
+            /* Write an executable program in C# that will hold the employee code and employee names available in 
+             an Organization using Collections.When the data is displayed it should be in a sorted manner.Choose an
+ appropiate type of Collection.*/
+            SortedList sl = new SortedList();
+            sl.Add(100, "Alfiya");
+            sl.Add(103, "Aleena");
+            sl.Add(102, "Joyna");
+            sl.Add(104, "Shammas");
+
+            foreach (var item in sl.Keys)
+            {
+                Console.WriteLine($"Emp_Id:{item} Name:{sl[item]}");
+            }
+        }
+    }
+}
---
+++ AssignmentDay6/Question5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay6
+{
+    class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public int std;
+        public char section;
+
+    }
+    internal class Question5
+    {
+        static void Main(string[] args)
+        {
+            /* Create a class with name student and store all the student details in an ArrayList and Display the
+             Details.*/
+            Student s = new Student() { id = 101, name = "Alfiya", age = 20, std = 10, section = 'A' };
+            Student s1 = new Student() { id = 102, name = "Angela", age = 21, std = 9, section = 'B' };
+            Student s2 = new Student() { id = 103, name = "Shiji", age = 22, std = 8, section = 'C' };
+            ArrayList al = new ArrayList();
+            al.Add(s);
+            al.Add(s1);
+            al.Add(s2);
+            foreach (Student i in al)
+            {
+                Console.WriteLine($"Id:{i.id} Name:{i.name} Age:{i.age} Std:{i.std} Section:{i.section}");
+            }
+        }
+    }
+   
+}
